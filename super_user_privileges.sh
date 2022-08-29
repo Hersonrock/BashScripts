686445bcbb0969ae4408596b65beea6b3b463d65
Hersonrock <hjpg20@gmail.com>
2022-08-29 18:48:45 -0500

    Created cleanup script for super_user_privileges.sh

--- super_user_privileges.sh
+++ super_user_privileges.sh
@@ -3,14 +3,24 @@
 #This is a script is focused on practicing concepts surrounding Super User Privileges and user creation
 # using su and su - 
 #manipulating /etc/sudoers/
-#creating a user with access to only specific commands 
+#creating a user with sudo access to only specific commands 
 #use of the which command 
 
 
-echo -e "This script will be creating a user named \033[36mTotoro\033[0m"
+echo -e "This script will be creating a user named \033[36mtotoro\033[0m"
 echo -e "This user will only have access to specific commands"
 
 printf "%-10s:%10s\n" "ifconfig" "netstat"
 printf "%-10s:%10s\n" "hostname" "arp"
 printf "%-10s:%10s\n" "route" "uname"
+
+#creating a dummy user
+
+useradd  totoro
+id totoro
+passwd totoro
+
+printf "totoro ALL=$(which ifconfig),$(which netstat)\n" >> /etc/sudoers
+
+echo "Command complete"
 
---
+++ super_user_privileges_clean.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#This script is meant for clean up.
+#Delete the sudoers file entry for user totoro
+#remove using using userdel -r to remove homedir and files
+
+head -n -1 /etc/sudoers > temp_sudoers 
+mv temp_sudoers /etc/sudoers
+userdel -r totoro
