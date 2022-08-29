e8f8f425fae65943b11a6b14c2e92b361187a4ca
Hersonrock <hjpg20@gmail.com>
2022-08-29 08:29:13 -0500

    Sending unecessary output of gitpush.sh to /dev/null

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 eval SSH_AUTH_SOCK=/tmp/ssh-XXXXXX2553bV/agent.6652; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=6653; export SSH_AGENT_PID;
-echo Agent pid 6653;
-ssh-add ~/.ssh/git_KEY
+SSH_AGENT_PID=6653; export SSH_AGENT_PID; 
+#echo Agent pid 6653;
+ssh-add ~/.ssh/git_KEY 2>/dev/null 
 echo -e "\033[36mAgentStart\nKeyAdded\033[0m"
