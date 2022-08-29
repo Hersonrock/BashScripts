623191acd2077d1562b36333b4cd314086484721
Hersonrock <hjpg20@gmail.com>
2022-08-29 18:30:17 -0500

    gitpush.sh script is not reliable, made changes to make it simpler while I debug it

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-eval SSH_AUTH_SOCK=/tmp/ssh-XXXXXX2553bV/agent.6652; export SSH_AUTH_SOCK;
-SSH_AGENT_PID=6653; export SSH_AGENT_PID; 
-#echo Agent pid 6653;
-ssh-add ~/.ssh/git_KEY 2>/dev/null 
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/git_KEY 
 echo -e "\033[36mAgentStart\nKeyAdded\033[0m"
