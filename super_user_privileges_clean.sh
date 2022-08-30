41398593f0a164218a3a476cfc904a0184a6582a
Hersonrock <hjpg20@gmail.com>
2022-08-29 21:05:45 -0500

    minor comment change on user_privileges script

--- super_user_privileges_clean.sh
+++ super_user_privileges_clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #This script is meant for clean up.
-#Delete the sudoers file entry for user totoro
-#remove using using userdel -r to remove homedir and files
+#Delete the sudoers file entry for user totoro.
+#using userdel -r to remove homedir and files
 
 head -n -1 /etc/sudoers > temp_sudoers 
 mv temp_sudoers /etc/sudoers
